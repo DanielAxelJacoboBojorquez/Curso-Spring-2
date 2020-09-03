346d852b6e9dcb2a145a63d6a30afb5445cb3320
Daniel Axel Jacobo Bojorquez <46801100+DanielAxelJacoboBojorquez@users.noreply.github.com>
2020-09-03 16:46:46 -0700

    Java Annotations

---
+++ src/es/pildoras/pruebaannotations/UsoAnnotations.java
@@ -0,0 +1,26 @@
+package es.pildoras.pruebaannotations;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UsoAnnotations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// 1.- Leer el xml de configuración
+		ClassPathXmlApplicationContext contexto=new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// 2.- Pedir un bean al contenedor
+		Empleados Antonio=contexto.getBean("ComercialExperimentado", Empleados.class);
+		
+		
+		// 3.- Usar el bean
+		System.out.println(Antonio.getInforme());
+		System.out.println(Antonio.getTareas());
+		
+		// 4.- Cerrar el contexto
+		contexto.close();
+		
+	}	
+
+}
