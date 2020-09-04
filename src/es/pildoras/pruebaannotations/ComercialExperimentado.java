026f31c18ad6c71100df334db9aba2b60ce293e8
Daniel Axel Jacobo Bojorquez <46801100+DanielAxelJacoboBojorquez@users.noreply.github.com>
2020-09-03 19:14:56 -0700

    Java Annotations - PostConstruct y PreDestroy

--- src/es/pildoras/pruebaannotations/ComercialExperimentado.java
+++ src/es/pildoras/pruebaannotations/ComercialExperimentado.java
@@ -1,13 +1,28 @@
 package es.pildoras.pruebaannotations;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
+//@Scope("prototype")
 public class ComercialExperimentado implements Empleados {
+	
+	//Ejecución de código después de creación del Bean
+	@PostConstruct
+	public void ejecutaDespuesCreacion() {
+		System.out.println("Ejecutado tras creación de Bean");
+	}
+	
+	//Ejecución de código después de apagado contenedor Spring
+	@PreDestroy
+	public void ejecutaAntesDestruccion() {
+		System.out.print("Ejecutado antes de la destrucción");
+	}
 	
 	/*@Autowired
 	public ComercialExperimentado(CreacionInformeFinanciero nuevoInforme) {
