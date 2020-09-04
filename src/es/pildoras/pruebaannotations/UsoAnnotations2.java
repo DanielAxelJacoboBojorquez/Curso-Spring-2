55dc177b3ae0a468553d7f73bfa4c1ddf9a0fe73
Daniel Axel Jacobo Bojorquez <46801100+DanielAxelJacoboBojorquez@users.noreply.github.com>
2020-09-03 19:34:05 -0700

    Java Annotations - Configuration

--- src/es/pildoras/pruebaannotations/UsoAnnotations2.java
+++ src/es/pildoras/pruebaannotations/UsoAnnotations2.java
@@ -1,5 +1,6 @@
 package es.pildoras.pruebaannotations;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class UsoAnnotations2 {
@@ -7,8 +8,9 @@ public class UsoAnnotations2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// 1.- Leer el xml de configuración
-		ClassPathXmlApplicationContext contexto=new ClassPathXmlApplicationContext("applicationContext.xml");
-		
+		//ClassPathXmlApplicationContext contexto=new ClassPathXmlApplicationContext("applicationContext.xml");
+		// 1.- Leer el class de configuración
+		AnnotationConfigApplicationContext contexto=new AnnotationConfigApplicationContext(EmpleadosConfig.class);
 		// 2.- Pedir un bean al contenedor
 		Empleados Antonio=contexto.getBean("comercialExperimentado", Empleados.class);
 		Empleados Lucia=contexto.getBean("comercialExperimentado", Empleados.class);		
